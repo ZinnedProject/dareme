bdc277859454e0ecdb012b5c0cec1587b8b00044
perkins2099 <a@aap.bz>
2012-07-30 18:43:08 -0500

    Fixed S3 Error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET'],       # required
 #    :region                 => 'US-Standard'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'dm.pf.images'                          # required
+  config.fog_directory  = 'dmpfimages'                          # required
   #config.fog_host       = 'http://localhost:3000'            # optional, defaults to nil
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
